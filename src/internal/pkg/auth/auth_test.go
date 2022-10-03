cbe8717901781613d878777094f55b2d81a60c52
Alimukhamed Tlekbai <ali.tlekbai@gmail.com>
2022-10-03 23:22:25 +0600

    fix: os.Setenv -> t.Setenv, use testza, change env value

--- src/internal/pkg/auth/auth_test.go
+++ src/internal/pkg/auth/auth_test.go
@@ -1,28 +1,24 @@
 package auth
 
 import (
-	"os"
-	"reflect"
 	"testing"
 
+	"github.com/MarvinJWendt/testza"
 	"github.com/MarvinJWendt/traefik-guardian/src/internal/pkg/config"
 )
 
 func TestGetValidPasswords(t *testing.T) {
-	os.Setenv("AUTH_HOST", "*")
-	os.Setenv("PASSWORDS", "plaintext:pass1|pass2|pass3")
-	if err := config.Initialize(); err != nil {
-		t.Fatalf("couldn't initialize config: %s", err)
-	}
+	t.Setenv("AUTH_HOST", "auth.example.com")
+	t.Setenv("PASSWORDS", "plaintext:pass1|pass2|pass3")
+
+	err := config.Initialize()
+	testza.AssertNoError(t, err)
 
 	expectedAlgo := "plaintext"
 	expectedPasswords := []string{"pass1", "pass2", "pass3"}
 
 	algorithm, passwords := GetValidPasswords()
-	if expectedAlgo != algorithm {
-		t.Fatalf("couldn't get algorithn:\ngot: %v\nwanted: %v\n", algorithm, expectedAlgo)
-	}
-	if !reflect.DeepEqual(expectedPasswords, passwords) {
-		t.Fatalf("couldn't get valid passwords:\ngot: %v\nwanted: %v\n", passwords, expectedPasswords)
-	}
+
+	testza.AssertEqual(t, expectedAlgo, algorithm, "algorithm doesn't match")
+	testza.AssertEqual(t, expectedPasswords, passwords, "passwords don't match")
 }
